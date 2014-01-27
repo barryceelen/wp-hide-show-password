d7d209652cf423a012ea77309dd500810ab146af
Barry Ceelen <barry@multipop.org>
2014-01-27 13:11:03 +0100

    Added check if has touch

--- js/public.js
+++ js/public.js
@@ -1,10 +1,14 @@
 (function ($) {
 	"use strict";
 	$(function () {
+		var hasTouch = false;
+		if ( ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch ) {
+			hasTouch = true;
+		}
 		$('#user_pass').hideShowPassword({
 			// Creates a wrapper and toggle element with minimal styles.
 			innerToggle: true,
-			touchSupport: true
+			touchSupport: hasTouch
 		});
 	});
 }(jQuery));
