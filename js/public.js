95b0e9f098e0b861b3a0b0bb088283be5a6b230a
Barry Ceelen <b@rryceelen.com>
2015-04-22 07:59:31 +0200

    Add checkbox

--- js/public.js
+++ js/public.js
@@ -1,14 +1,22 @@
 (function ($) {
 	"use strict";
 	$(function () {
-		var bool = false;
+		var el = $('#user_pass');
+		var innerToggle = (1 == hideShowPasswordVars.innerToggle) ? true : false;
+		var enableTouchSupport = false;
 		if ( ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch ) {
-			bool = true;
+			enableTouchSupport = true;
 		}
-		$('#user_pass').hideShowPassword(false, true, {
+		el.hideShowPassword( false, innerToggle, {
 			toggle: {
-				touchSupport: bool
+				touchSupport: enableTouchSupport
 			}
 		});
+		if ( false == innerToggle ) {
+			var checkbox = $('<label class="hideShowPassword-checkbox"><input type="checkbox" /> '+hideShowPasswordVars.checkboxLabel+'</label>').insertAfter(el.parent('label'));
+			checkbox.on( 'change.hideShowPassword', 'input[type="checkbox"]', function() {
+				el.togglePassword().focus();
+			})
+		}
 	});
 }(jQuery));
